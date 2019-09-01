d516261f1e7e46931fc32981f353eed7e1278288
Andre <astevens009@users.noreply.github.com>
2019-08-31 22:35:17 -0400

    Clean up - Remove extraneous comments

--- MobilePhoneDemo/src/org/kemetkoder/astevens/MobilePhone.java
+++ MobilePhoneDemo/src/org/kemetkoder/astevens/MobilePhone.java
@@ -13,22 +13,12 @@ public class MobilePhone {
 	}
 	
 	public void removeContact() {
-		
-//		String firstName = "";
-//		String lastName = "";
-		
+
 		String contactName = "";
 	
 		if(this.getUserContacts().isEmpty())
 			System.out.println("No contacts available.");
-		else {
-			// TODO: get the contact to remove
-//			System.out.println("Please enter the contact's first name:");
-//			firstName = userInput.nextLine(); 
-//			
-//			System.out.println("Please enter the contact's last name:");
-//			lastName = userInput.nextLine(); 
-			
+		else {			
 			System.out.println("Please enter contact's name:");
 			contactName = userInput.nextLine();
 					
@@ -90,21 +80,6 @@ public class MobilePhone {
 		return contactIndex;
 	}
 
-//	public void displayContacts() {
-//		
-//		if(this.getUserContacts().isEmpty())
-//			System.out.println("No contacts available.");
-//		else {
-//			System.out.println("Contact List:\n");
-//			for(Contacts c : this.getUserContacts())
-//				System.out.println(c.getLastName() + ", " + 
-//						c.getFirstName() + "\n" +
-//						c.getPhoneNumber() + "\n");
-//		}
-//		
-//	}
-	
-
 	public void displayContacts() {
 		
 		if(this.getUserContacts().isEmpty())
@@ -124,32 +99,6 @@ public class MobilePhone {
 		System.out.println("Contact Info:\nName: " + contactToDisplay.getName() +
 				"\nPhone Number: " + contactToDisplay.getPhoneNumber());
 	}
-
-//	public void addContact() {
-//		char response = 'Y';
-//		Contacts contact;
-//		
-//		do {
-//			contact = new Contacts();
-//			
-//			System.out.println("Enter the contact's first name: ");
-//			contact.setFirstName(userInput.nextLine());
-//			
-//			System.out.println("Enter the contact's last name: ");
-//			contact.setLastName(userInput.nextLine());
-//			
-//			System.out.println("Enter the contact's phone number: ");
-//			contact.setPhoneNumber(userInput.nextLine());
-//			
-//			this.userContacts.add(contact);
-//			
-//			System.out.println("Add another? (Y/N)");
-//			response = userInput.nextLine().charAt(0);
-//		
-//		} while(response == 'Y' || response == 'y');
-//
-//	}
-
 
 	public void addContact() {
 		char response = 'Y';
